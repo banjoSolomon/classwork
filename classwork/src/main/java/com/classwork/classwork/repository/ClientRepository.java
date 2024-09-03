4bad4cdea316bb4b5fde0187ad5246b341e120d3
banjoSolomon <ayomidebanjo68@gmail.com>
2024-09-03 13:41:58 +0100

    update

--- classwork/src/main/java/com/classwork/classwork/repository/ClientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.classwork.classwork.repository;
-
-import model.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientRepository extends JpaRepository<Client, Long> {
-}
--- classwork/src/main/java/com/classwork/classwork/service/ClientService.java
+++
@@ -1,8 +0,0 @@
-package com.classwork.classwork.service;
-
-import com.classwork.classwork.request.RegisterUserRequest;
-import com.classwork.classwork.request.RegisterUserResponse;
-
-public interface ClientService {
-    RegisterUserResponse registerUser(RegisterUserRequest registerUserRequest);
-}
